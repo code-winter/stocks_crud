c87f435fc028b81ec4ebdecc0cf302f94a7c3c0b
Dmitry Popov <ant1h4t3rz@gmail.com>
2022-03-17 20:53:44 +0900

    override to queryset

--- logistic/views.py
+++ logistic/views.py
@@ -2,7 +2,7 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.viewsets import ModelViewSet
 
-from logistic.models import Product, Stock
+from logistic.models import Product, Stock, StockProduct
 from logistic.serializers import ProductSerializer, StockSerializer
 
 
@@ -16,6 +16,13 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    filter_backends = [DjangoFilterBackend, SearchFilter]
-    filterset_fields = ['products']
+    filter_backends = [DjangoFilterBackend]
+    # filterset_fields = ['products']
+
+    def get_queryset(self):
+        title = self.request.query_params.get('products')
+        my_query = Product.objects.get(title__icontains=title)
+        product_id = my_query.id
+        queryset = Stock.objects.filter(products=product_id)
+        return queryset
 
